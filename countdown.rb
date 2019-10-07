d21f28304abcb8c5105ccd6904be6b090d4a5961
Phil Billingsby <paulbillingsby@gmail.com>
2019-10-07 20:35:08 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,7 +1,7 @@
 #write your code here
 def countdown(number)
   while number > 0
-    return "#{number} SECOND(S)!\n"
+    return "#{number} SECOND(S)!"
     number -= 1
   end
   return "HAPPY NEW YEAR!"
